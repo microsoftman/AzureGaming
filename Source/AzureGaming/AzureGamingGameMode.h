c43d2ed7e7964adf47ddbc2da8b901bbf4a195e1
Ben Humphrey <behumphr@microsoft.com>
2020-09-15 04:24:18 -0500

    Start of Azure Gaming project, started from Third Person project in Unreal with C++ and starter content

---
+++ Source/AzureGaming/AzureGamingGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AzureGamingGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAzureGamingGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAzureGamingGameMode();
+};
+
+
+
